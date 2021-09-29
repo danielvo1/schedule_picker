788b1f7da0acb6f018cbc81d43eebc4dea7dc846
Matt Weichel <maweiche@gmail.com>
2021-09-29 11:47:34 -0700

    added bootstrap link and js link, modal working properly, but not styled

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var time = document.querySelector('#cur_time');
 
 setInterval(function() { 
     time.textContent = moment().format('MM Do YYYY, h:mm:ss a');
-    console.log(time);
+    // console.log(time);
 } , 1000 );
 
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
+$('#exampleModal').on('click', function () {
+    $('#exampleModal').trigger('focus')
+
+    console.log("thisis what clicked", );
   })
